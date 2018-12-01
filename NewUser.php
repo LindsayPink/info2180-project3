765adf3693ca5dd131aae118353a5de5fe3b431b
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-01 06:53:58 +0000

    created empty NewUser file and coded for a new user form

---
+++ NewUser.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$password = $_POST['Password'];
+	$email = $_POST['email'];
+	$telephone = $_POST['telephone'];
+	$submit = $_POST['submit'];
+
+	include_once 'dashboard.php';
+} else {
+	include_once 'no-submission.view.php';
+	// You could also display the form again or redirect the user to the form.
+	// header('Location: index.html');
+}
