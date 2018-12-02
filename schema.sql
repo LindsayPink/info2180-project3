4388276dd9030aa3482aa562b47388ab03902c64
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-02 21:34:26 +0000

    added sentences to add 7 jobs to db

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ date_joined DATE
 );
 
 CREATE TABLE Jobs(
-id INT PRIMARY KEY,
+id INT PRIMARY KEY AUTO_INCREMENT,
 job_title VARCHAR(32) DEFAULT NULL,
 job_description TEXT DEFAULT NULL,
 category VARCHAR(32) DEFAULT NULL,
@@ -28,3 +28,15 @@ job_id  INT,
 user_id  INT,
 date_applied DATE
 );
+
+INSERT INTO Users (email, password) VALUES
+("admin", "password123");
+
+INSERT INTO Jobs (company_name, job_title, category, date_posted) VALUES
+('Jamaica Gleaner', 'Production Marketing Manager', 'Sales & Marketing', '2018-11-03'),
+('UWI - MITS', 'Software Engineer', 'Programming', '2018-11-02'),
+('NCB', 'Business Analyst - Scrum Master', 'Business & Management', '2018-11-01'),
+('Jamaica Yellow Pages', 'UX/UI Designer', 'Design', '2018-10-20'),
+('UWI - Busary', 'Director Customer Support', 'Customer Support', '2018-10-20'),
+('Sagicor Bank', 'Senior Systems Engineer', 'DevOps & Sysadmin', '2018-10-20');
+('Basecamp', 'Software Engineer', 'Programming', '2018-05-01');
