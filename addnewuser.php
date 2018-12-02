450f06915ad6f40c3c6c3d8399d3f8eefe219b55
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-02 21:47:37 +0000

    replaces New_User.php, displays form for adding new user

---
+++ addnewuser.php
@@ -0,0 +1,18 @@
+<h2 id="view">Add New User</h2>
+
+<!--<div id="details">
+    <form action="login.php" method="post">
+        <div class="form-group">
+            <label for="text">Email address</label>
+            <input type="text" name="email" id="email" class="form-control" required/>
+        </div>
+        
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password"  id="password" class="form-control" required/>
+        </div>
+        
+        <button id="login">Search</button>
+    
+    </form>
+</div>
