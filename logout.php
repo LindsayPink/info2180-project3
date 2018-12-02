fed9f94e1e847cba6d91303f8a4e18e26a950415
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-02 21:52:42 +0000

    tells user they have been logged out and provides form for login again

---
+++ logout.php
@@ -0,0 +1,18 @@
+<h2 id="view">Log out successful</h2>
+
+<!--<div id="details">
+    <form action="login.php" method="post">
+        <div class="form-group">
+            <label for="text">Email address</label>
+            <input type="text" name="email" id="email" class="form-control" required/>
+        </div>
+        
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password"  id="password" class="form-control" required/>
+        </div>
+        
+        <button id="login">Log In</button>
+    
+    </form>
+</div>
