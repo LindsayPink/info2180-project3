6f25d1c8ef71c1d16616405add6083d8289138a9
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-03 02:08:09 +0000

    form details added

--- postjob.php
+++ postjob.php
@@ -1,1 +1,41 @@
 <h2 id="view">New Job</h2>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label for="jtitle">Job Title</label>
+        <input type="text" name="jtitle" id="jtitle" class="form-control" placeholder="e.g. Senior Designer or Product Manager" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="jdescrip">Job Description</label>
+        <input type="text" name="jdescrip"  id="jdescrip" class="form-control" placeholder="Tell potential employees what this job is about." required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="cat">Category</label>
+        <input type="text" name="cat"  id="cat" class="form-control" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="comp">Company</label>
+        <input type="text" name="comp"  id="comp" class="form-control" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="jloc">Job Location</label>
+        <input type="text" name="jloc"  id="jloc" class="form-control" placeholder="e.g. Kingston, Jamaica" required/>
+    </div>
+    
+    <br>
+    
+    <button id="submitU">Submit</button>
+
+</form>
