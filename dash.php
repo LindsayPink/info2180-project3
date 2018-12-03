4e4e5cb43396a1560021fc3003fb8a4448f4f086
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-03 03:00:20 +0000

    gets sql data for specific user

---
+++ dash.php
@@ -0,0 +1,20 @@
+<?php
+$host = getenv('IP');
+
+$username = getenv('C9_USER');
+
+$password = '';
+
+$dbname = 'scheme';
+
+$userE = $_POST['userE'];
+$userP = $_POST['userP'];
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$conn->query('SELECT * FROM Jobs');
+
+$jobs = $conn->query('SELECT * FROM Jobs LIMIT 8');
+
+
+$statement = $conn->query("SELECT * FROM JobsAppliedFor");
+$jobsAppliedFor = $statement->fetchAll(PDO::FETCH_ASSOC);
