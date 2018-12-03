ed5507b510b66b23fef4ea96a5440b430da2ff46
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-03 03:01:43 +0000

    displays log in form

---
+++ home.php
@@ -0,0 +1,18 @@
+<h2 id="view">Log in</h2>
+
+<div id="details">
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="text" name="email" id="email" class="form-control" placeholder="someone@example.com" required/>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password"  id="password" class="form-control" required/>
+        </div>
+        <br>
+        <button id="login">Log In</button>
+    
+    </form>
+</div>
