6c4503f6f6c5247720c1f383a78757b027e9c10c
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-03 02:56:22 +0000

    handles log in attempt

--- login.php
+++ login.php
@@ -1,1 +1,12 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  if (!isset($_POST['email']) or !isset($_POST['password']) or
+  empty($_POST['email']) or empty($_POST['password'])) {
+    echo "What?!?!? You don't have an email address or password?";
+  } else {
+      //echo "<a class='home' href='dashboard.php'>Home</a>";
+   echo "Your email is: <strong>" . $_POST['email'] . "</strong> and your password is: <strong>" . $_POST['password'] . "</strong>.";
+  }
+}
