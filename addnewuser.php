d71164e5cbfeb2f3ef9ccf983a993a5e51884c18
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-03 01:52:09 +0000

    form details added

--- addnewuser.php
+++ addnewuser.php
@@ -1,18 +1,41 @@
-<h2 id="view">Add New User</h2>
+<h2 id="view">New User</h2>
 
-<!--<div id="details">
-    <form action="login.php" method="post">
-        <div class="form-group">
-            <label for="text">Email address</label>
-            <input type="text" name="email" id="email" class="form-control" required/>
-        </div>
-        
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password"  id="password" class="form-control" required/>
-        </div>
-        
-        <button id="login">Search</button>
+<form action="" method="post">
+    <div class="form-group">
+        <label for="text">First name</label>
+        <input type="text" name="email" id="fname" class="form-control" placeholder="John" required/>
+    </div>
     
-    </form>
-</div>
+    <br>
+    
+    <div class="form-group">
+        <label for="password">Last name</label>
+        <input type="text" name="lname"  id="lname" class="form-control" placeholder="Doe" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="text" name="password"  id="password" class="form-control" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="password">Email</label>
+        <input type="email" name="email"  id="email" class="form-control" placeholder="someone@example.com" required/>
+    </div>
+    
+    <br>
+    
+    <div class="form-group">
+        <label for="password">Telephone</label>
+        <input type="tel" name="tel"  id="tel" class="form-control" placeholder="876-555-0123" required/>
+    </div>
+    
+    <br>
+    
+    <button id="submitU">Submit</button>
+
+</form>
