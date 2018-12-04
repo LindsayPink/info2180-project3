e10fcdbe0cdc67651d0e0f5ce001c3fed6659990
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-04 17:21:01 +0000

    modified, purpose still being decided

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+require 'dashboard.php';
+/*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   if (!isset($_POST['email']) or !isset($_POST['password']) or
   empty($_POST['email']) or empty($_POST['password'])) {
@@ -9,4 +9,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       //echo "<a class='home' href='dashboard.php'>Home</a>";
    echo "Your email is: <strong>" . $_POST['email'] . "</strong> and your password is: <strong>" . $_POST['password'] . "</strong>.";
   }
-}
+}*/
