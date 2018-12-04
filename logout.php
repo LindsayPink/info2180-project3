34d942a2229df40e74227ab9bc7968e68b01363a
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-04 18:10:26 +0000

    now displays login form

--- logout.php
+++ logout.php
@@ -1,17 +1,17 @@
 <h2 id="view">Log out successful</h2>
 
-<!--<div id="details">
-    <form action="login.php" method="post">
+<div id="details">
+    <form action="" method="post">
         <div class="form-group">
-            <label for="text">Email address</label>
-            <input type="text" name="email" id="email" class="form-control" required/>
+            <label for="email">Email address</label>
+            <input type="text" name="email" id="email" class="form-control" placeholder="someone@example.com" required/>
         </div>
-        
+        <br>
         <div class="form-group">
             <label for="password">Password</label>
             <input type="password" name="password"  id="password" class="form-control" required/>
         </div>
-        
+        <br>
         <button id="login">Log In</button>
     
     </form>
