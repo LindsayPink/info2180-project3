95de15955721917cc28e8ef1d5d62db59df6ca94
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-12-04 17:23:22 +0000

    initial sql query code added

--- NewUser.php
+++ NewUser.php
@@ -1,4 +1,31 @@
-<?php /*
+<?php 
+
+$host = getenv('IP');
+
+$username = getenv('C9_USER');
+
+$password = '';
+
+$dbname = 'scheme';
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$pwd = $_POST['password'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$conn = $dbname ->prepare('INSERT INTO Users (firstname, lastname, password, telephone, email) VALUES
+($fname, $lname, $pwd, $email, $tel');
+
+$conn->query('SELECT * FROM Jobs');
+
+$jobs = $conn->query('SELECT * FROM Jobs LIMIT 5');
+
+
+$statement = $conn->query("SELECT * FROM JobsAppliedFor");
+$jobsAppliedFor = $statement->fetchAll(PDO::FETCH_ASSOC);
+/*
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
